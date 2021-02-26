133adb27ddcf25e6a65308a1567fc33eaef525f1
airtonfranco <x572493@gruposantander.com>
2021-02-26 16:18:10 -0300

    install sqlite3

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,25 @@ import express from 'express';
 
 const app = express();
 
+/*
+GET => BUSCAR
+POST => SALVAR
+PUT => ALTERAR
+DELETE => DELETAR
+PATCH => ALTERAÇÃO ESPECIFICA
+*/
+
+//http://localhost:3333/users
+app.get('/users', (req, res) => {
+    return res.json({
+        mensagem: 'Ola Dev'
+    })
+});
+
+app.post('/', (req, res) => {
+    return res.json({
+        mensagem: 'Os dados foram salvo com sucesso!!!'
+    })
+})
+
 app.listen(3333, () => console.log('server is runer'))
