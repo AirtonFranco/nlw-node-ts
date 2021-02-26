0c0fc2f289e36b84e26cbea4e818becd53520494
airtonfranco <x572493@gruposantander.com>
2021-02-26 17:16:14 -0300

    create controllers

--- src/database/migrations/1614368683321-CreateUsers.ts
+++ src/database/migrations/1614368683321-CreateUsers.ts
@@ -1,11 +1,39 @@
+import { table } from "console";
+import { query } from "express";
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class CreateUsers1614368683321 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new table({
+                nome: "users",
+                colums: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true 
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
